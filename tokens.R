a7d29858a7cb1de06a4331ae95e21ab484dff1f4
welkiej7 <94485797+welkiej7@users.noreply.github.com>
2022-06-22 15:20:16 +0300

    Create tokens.R

---
+++ tokens.R
@@ -0,0 +1,13 @@
+appname <- "Turkey_Misinformation"
+
+api_key <- "9f7qpxZcsny4Ju4eVcVPRN3KW"
+api_secret_key <- "bfAFyvpgfEv8Nf5coBMCmoEvHjt3XJYQ96YlSSRywp79UQHT0l"
+access_token<- "1219650013502017543-FIfCEruV9OmayxXOIDz1pxVxFgNDvR"
+access_secret <- "hRzSMwgC7oGCYgRo4jCB4yvNnceYV4qQHXeA48o0pHN5Y"
+
+
+create_token(app = "Turkey_Misinformation",
+             access_token = access_token,
+             access_secret = access_secret,
+             consumer_key = api_key,
+             consumer_secret = api_secret_key)
